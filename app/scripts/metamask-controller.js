b5a0af152720972b1a14ce1675759310c049f0f7
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-02-21 17:35:41 +0000

    remove provider from stx ctrl constructor params

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1760,7 +1760,6 @@ export default class MetamaskController extends EventEmitter {
         ),
         confirmExternalTransaction:
           this.txController.confirmExternalTransaction.bind(this.txController),
-        provider: this.provider,
         trackMetaMetricsEvent: this.metaMetricsController.trackEvent.bind(
           this.metaMetricsController,
         ),
